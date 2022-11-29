2c5ec0358c348db9ad62e2fdbfa971a2e637f66c
Hilarie <109528810+hnoes@users.noreply.github.com>
2022-11-29 10:12:31 -0500

    Add files via upload

---
+++ oo-challenge.js
@@ -0,0 +1,58 @@
+// OO challenge
+
+class Vehicle {
+    constructor (make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    honk () {
+        return "Beep!";
+    }
+
+    toString(){
+        return `The vehicle is a ${this.make} ${this.model} from ${this.year}.`;
+    }
+}
+
+// Part 2 
+// car class
+
+class Car extends Vehicle {
+    constructor (make, model, year) {
+        super(make, model, year);
+        this.numWheels = 4;
+    }
+}
+
+// motorcycle class
+
+class Motorcycle extends Vehicle {
+    constructor (make, model, year) {
+        super(make, model, year);
+        this.numWheels = 2;
+    }
+
+    revEngine() {
+        return "VROOOOM!!";
+    }
+}
+
+class Garage {
+    constructor(capacity) {
+        this.vehicles = [];
+        this.capacity = capacity;
+    }
+
+    add(newVehicle){
+        if (!(newVehicle instanceof Vehicle)) {
+            return "Only vehicles are allowed in here!";
+        }
+        if (this.vehicles.length >= this.capacity) {
+            return "Sorry, we are full.";
+        }
+        this.vehicles.push(newVehicle);
+        return "Vehicle added!";
+    }
+}
